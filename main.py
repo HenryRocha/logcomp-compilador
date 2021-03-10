032fada7d20e17dad885b5a70e0d9798ea9ea783
Henry Rocha <henryrocha@protonmail.com>
2021-03-10 00:02:36 -0300

    Remove unused var

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from parser import Parser
 
 
 if __name__ == "__main__":
-    VALID_CHARACTERS = ["1", "2", "3", "4",
-                        "5", "6", "7", "8", "9", "0", "+", "-", " "]
-
     # Get the first argument, parse it as a string and remove any traling whitespaces,
     # at the end or the beginning of the string.
     originalMath = str(sys.argv[1]).strip()
